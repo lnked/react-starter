a7de3d2e777f318ad4436bf3e815d6e8e7e030a9
ed <ed.proff@gmail.com>
2017-05-21 16:46:22 +0300

    update

--- app/components/form/index.jsx
+++ app/components/form/index.jsx
@@ -18,3 +18,85 @@
 // }
 
 // export default Form
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { FormWithConstraints, FieldFeedbacks, FieldFeedback } from 'react-form-with-constraints';
+
+class MyForm extends FormWithConstraints {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: 'john@doe.com',
+      password: '',
+      passwordConfirm: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const target = e.currentTarget;
+    this.setState({
+      [target.name]: target.value
+    });
+    super.handleChange(e);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    super.handleSubmit(e);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} noValidate>
+        <div>
+          <label>Username</label>
+          <input type="email" name="username"
+                 value={this.state.username} onChange={this.handleChange}
+                 required />
+          <FieldFeedbacks for="username">
+            <FieldFeedback when="*" />
+          </FieldFeedbacks>
+        </div>
+
+        <div>
+          <label>Password</label>
+          <input type="password" name="password"
+                 value={this.state.password} onChange={this.handleChange}
+                 pattern=".{5,}" required />
+          <FieldFeedbacks for="password" show="all">
+            <FieldFeedback when="valueMissing" />
+            <FieldFeedback when="patternMismatch">Should be at least 5 characters long</FieldFeedback>
+            <FieldFeedback when={value => !/\d/.test(value)} warning>Should contain some numbers</FieldFeedback>
+            <FieldFeedback when={value => !/[a-z]/.test(value)} warning>Should contain some small letters</FieldFeedback>
+            <FieldFeedback when={value => !/[A-Z]/.test(value)} warning>Should contain some capital letters</FieldFeedback>
+            <FieldFeedback when={value => !/\W/.test(value)} warning>Should contain some special characters</FieldFeedback>
+          </FieldFeedbacks>
+        </div>
+
+        <FieldFeedbacks for="password">
+          <FieldFeedback when="valueMissing" />
+          <FieldFeedback when="patternMismatch">Should be at least 5 characters long</FieldFeedback>
+        </FieldFeedbacks>
+
+        <div>
+          <label>Confirm Password</label>
+          <input type="password" name="passwordConfirm"
+                 value={this.state.passwordConfirm} onChange={this.handleChange}
+                 required />
+          <FieldFeedbacks for="passwordConfirm">
+            <FieldFeedback when="*" />
+            <FieldFeedback when={value => value !== this.state.password}>Not the same password</FieldFeedback>
+          </FieldFeedbacks>
+        </div>
+
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
