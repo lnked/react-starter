e3118500619e706efeb9d693fbeea027b0d80464
ed <ed.proff@gmail.com>
2017-05-25 23:14:03 +0300

    update

--- app/settings/routes.jsx
+++ app/settings/routes.jsx
@@ -3,3 +3,24 @@
 const routes = [];
 
 module.exports.config = routes;
+
+// <Router>
+// <div>
+//   <Link to="/">Home</Link>{' '}
+//   <Link to={{pathname: '/about'}}>About</Link>{' '}
+//   <Link to="/contact">Contact</Link>
+  
+//   <Switch>
+//     <Route path="/" component={Home} />
+//     <Route path="/about" component={About} />
+//     <Route
+//       path="/contact"
+//       render={() => <h1>Contact Us</h1>} />
+//     <Route path="/blog" children={({match}) => (
+//       <li className={match ? 'active' : ''}>
+//         <Link to="/blog">Blog</Link>
+//       </li>)} />
+//     <Route render={() => <h1>Page not found</h1>} />
+//   </Switch>
+// </div>
+// </Router>
