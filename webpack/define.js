32731df3b20352d7de513678b4596e2f2bcc1ef9
ed <ed.proff@gmail.com>
2017-06-02 21:01:36 +0300

    update

--- webpack/define.js
+++ webpack/define.js
@@ -9,23 +9,18 @@ const _deploy_ = '/react-starter/';
 const isDev = process.env.NODE_ENV === 'development';
 const isPro = process.env.NODE_ENV === 'production' || !isDev;
 
-const isDeploy  = process.argv.includes('--deploy');
-const isRelease = process.argv.includes('--release');
-const isAnalyze = process.argv.includes('--analyze');
-const isCompress = isPro || isRelease || isAnalyze;
-
-console.log('isDeploy: ', isDeploy)
-console.log('isRelease: ', isRelease)
-console.log('isAnalyze: ', isAnalyze)
-console.log('isCompress: ', isCompress)
+const isDeploy  = process.argv.includes('deploy');
+const isAnalyze = process.argv.includes('analyze');
+const isRelease = process.argv.includes('release') || isAnalyze;
+const isAssemply = isPro || isRelease || isAnalyze;
 
 module.exports = {
     rs_generate_css: false,
     rs_root: _root_,
     rs_dist: _dist_,
     rs_output_path: isDeploy ? _deploy_ : '',
-    rs_environment: isCompress ? 'production' : 'development',
-    rs_production: isCompress,
+    rs_environment: isAssemply ? 'production' : 'development',
+    rs_production: isAssemply,
     rs_development: isDev,
     rs_analyzer: isAnalyze,
     rs_release: isRelease
