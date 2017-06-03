cadc31fff31a466ed16e96f4a58e98e1c8bd9046
ed <ed.proff@gmail.com>
2017-06-03 03:55:48 +0300

    update

--- app/layouts/index.js
+++ app/layouts/index.js
@@ -1,7 +1,9 @@
+import PureLayout from 'layouts/pure-layout'
 import CoreLayout from 'layouts/core-layout'
 import AuthLayout from 'layouts/auth-layout'
 
 export {
+    PureLayout,
     CoreLayout,
     AuthLayout
 }
---
+++ app/layouts/pure-layout/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { oneOfType, object, string, array } from 'prop-types'
+import css from './styles.scss'
+
+import { SvgFixer } from 'utils'
+
+export default class PureLayout extends Component {
+
+    static propTypes = {
+        children: oneOfType([
+            object,
+            string,
+            array
+        ])
+    }
+
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            title: 'React Starter App'
+        }
+    }
+
+    componentWillMount () {
+        document.title = this.state.title
+        window.prerenderReady = true
+    }
+
+    componentDidMount () {
+        SvgFixer()
+    }
+
+    render () {
+        return (
+            <div className={css.layout}>
+                { this.props.children }
+            </div>
+        )
+    }
+}
