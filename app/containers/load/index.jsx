f626c371943746a6d1922f1664be6eb23839e337
ed <ed.proff@gmail.com>
2017-06-26 18:57:31 +0300

    update

--- app/containers/load/index.jsx
+++ app/containers/load/index.jsx
@@ -5,6 +5,7 @@ import { Posts } from 'segments'
 import 'whatwg-fetch'
 
 export default class Load extends Component {
+
     static propTypes = {
         subreddit: string,
         posts: oneOfType([
