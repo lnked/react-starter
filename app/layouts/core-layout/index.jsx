38a5ff1e0abb7ffcd0ed652cf7251d6d7c5c4998
ed <ed.proff@gmail.com>
2017-07-14 14:28:50 +0300

    update

--- app/layouts/core-layout/index.jsx
+++ app/layouts/core-layout/index.jsx
@@ -40,6 +40,28 @@ export default class CoreLayout extends PureComponent {
     }
 
     render () {
+        // import {Helmet} from "react-helmet"
+        // <div className="application">
+        //     <Helmet>
+        //         <meta charSet="utf-8" />
+        //         <title>My Title</title>
+        //         <link rel="canonical" href="http://mysite.com/example" />
+        //     </Helmet>
+        // </div>
+
+        // <Parent>
+        //     <Helmet>
+        //         <title>My Title</title>
+        //         <meta name="description" content="Helmet application" />
+        //     </Helmet>
+
+        //     <Child>
+        //         <Helmet>
+        //             <title>Nested Title</title>
+        //             <meta name="description" content="Nested component" />
+        //         </Helmet>
+        //     </Child>
+        // </Parent>
         return (
             <Router>
                 <div className={css.layout}>
