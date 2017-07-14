62f9ff5cbe25d59b5488c648463f91b18eeea59b
ed <ed.proff@gmail.com>
2017-07-15 02:22:44 +0300

    upgrade

--- app/components/image-crop/index.jsx
+++ app/components/image-crop/index.jsx
@@ -6,6 +6,37 @@ import { string } from 'prop-types'
 // http://dropsofserenity.github.io/react-avatar-cropper/
 // http://roadmanfong.github.io/react-cropper/example/
 
+import { classNames } from 'classnames'
+
+const btnClass = classNames({
+    btn: true,
+    'btn-pressed': true,
+    'btn-over': true
+})
+
+console.log(btnClass)
+
+// var arr = ['b', { c: true, d: false }];
+// classNames('a', arr); // => 'a b c' 
+
+// let buttonType = 'primary';
+// classNames({ [`btn-${buttonType}`]: true });
+
+// classNames('foo', 'bar'); // => 'foo bar' 
+
+// classNames('foo', 'bar'); // => 'foo bar' 
+// classNames('foo', { bar: true }); // => 'foo bar' 
+// classNames({ 'foo-bar': true }); // => 'foo-bar' 
+// classNames({ 'foo-bar': false }); // => '' 
+// classNames({ foo: true }, { bar: true }); // => 'foo bar' 
+// classNames({ foo: true, bar: true }); // => 'foo bar' 
+ 
+// // lots of arguments of various types 
+// classNames('foo', { bar: true, duck: false }, 'baz', { quux: true }); // => 'foo bar baz quux' 
+ 
+// // other falsy values are just ignored 
+// classNames(null, false, 'bar', undefined, 0, 1, { baz: null }, ''); // => 'bar 1' 
+
 export default class imageCrop extends Component {
     static propTypes = {
         name: string
