7aa9e8eb3fd90228eaafab85c4be711c93a7597f
ed <ed.proff@gmail.com>
2017-11-10 00:19:07 +0300

    upgrade

--- app/containers/component-page/index.jsx
+++ app/containers/component-page/index.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import css from './styles.scss'
 
-import VirtualizedSelect from 'react-virtualized-select'
+// import VirtualizedSelect from 'react-virtualized-select'
 
-import 'react-select/dist/react-select.css'
+// import 'react-select/dist/react-select.css'
 // import 'react-virtualized/styles.css'
 // import 'react-virtualized-select/styles.css'
 
@@ -11,7 +11,7 @@ import components from './request.json'
 
 import { CustomComponent } from 'components'
 
-import Noty from 'noty'
+// import Noty from 'noty'
 
 export default class ComponentPage extends Component {
     constructor (props) {
@@ -40,52 +40,53 @@ export default class ComponentPage extends Component {
     }
 
     render () {
-        const options = [
-            { label: 'One', value: 1 },
-            { label: 'Two', value: 2 },
-            { label: 'Three', value: 3, disabled: true }
-        ]
+        // const options = [
+        //     { label: 'One', value: 1 },
+        //     { label: 'Two', value: 2 },
+        //     { label: 'Three', value: 3, disabled: true }
+        // ]
 
-        new Noty({
-            text: 'Notification text'
-        }).show()
+        // new Noty({
+        //     text: 'Notification text'
+        // }).show()
 
-        new Noty({
-            type: 'warning',
-            layout: 'topRight',
-            theme: 'mint', /* relax, mint, metroui */
-            text: 'Interactive example',
-            timeout: 5000,
-            progressBar: true,
-            closeWith: ['click', 'button'],
-            animation: {
-                open: 'noty_effects_open',
-                close: 'noty_effects_close'
-            },
-            id: false,
-            force: false,
-            killer: false,
-            queue: 'global',
-            container: false,
-            buttons: [],
-            sounds: {
-                sources: [],
-                volume: 1,
-                conditions: []
-            },
-            titleCount: {
-                conditions: []
-            },
-            modal: false
-        }).show()
+        // new Noty({
+        //     type: 'warning',
+        //     layout: 'topRight',
+        //     theme: 'mint', /* relax, mint, metroui */
+        //     text: 'Interactive example',
+        //     timeout: 5000,
+        //     progressBar: true,
+        //     closeWith: ['click', 'button'],
+        //     animation: {
+        //         open: 'noty_effects_open',
+        //         close: 'noty_effects_close'
+        //     },
+        //     id: false,
+        //     force: false,
+        //     killer: false,
+        //     queue: 'global',
+        //     container: false,
+        //     buttons: [],
+        //     sounds: {
+        //         sources: [],
+        //         volume: 1,
+        //         conditions: []
+        //     },
+        //     titleCount: {
+        //         conditions: []
+        //     },
+        //     modal: false
+        // }).show()
+
+        // <VirtualizedSelect
+        //     options={options}
+        //     onChange={(selectValue) => this.setState({ selectValue })}
+        //     value={this.state.selectValue}
+        // />
 
         return (
             <div>
-                <VirtualizedSelect
-                    options={options}
-                    onChange={(selectValue) => this.setState({ selectValue })}
-                    value={this.state.selectValue}
-                />
                 { this.renderComponents() }
             </div>
         )
