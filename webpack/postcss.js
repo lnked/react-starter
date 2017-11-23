4075e11131cf8290d3af04f7fefeb60b524de720
ed <ed.proff@gmail.com>
2017-11-23 21:46:12 +0300

    change postcss

--- webpack/postcss.js
+++ webpack/postcss.js
@@ -5,8 +5,7 @@ const postcss = [];
 const define = require('./define');
 
 postcss.push(
-    require('postcss-comment'),
-    // require('postcss-strip-inline-comments'),
+    require('postcss-strip-inline-comments'),
     require('stylelint'),
     require('postcss-bem-linter'),
     require('postcss-import')({
