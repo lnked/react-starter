d5ab631890437285fa4183fea2e92f002b9ec457
ed <ed.proff@gmail.com>
2017-12-10 02:30:40 +0300

    update

--- webpack/rules/images.js
+++ webpack/rules/images.js
@@ -15,82 +15,42 @@ const rules = [
                     digest: 'hex',
                     name: 'images/[name]-[hash:5].[ext]'
                 }
-            },
-            {
-                loader: 'image-webpack-loader',
-                options: {
-                    bypassOnDebug: true,
-                    mozjpeg: {
-                        quality: 70,
-                        progressive: true
-                    },
-                    pngquant:{
-                        quality: '65-90',
-                        speed: 4
-                    },
-                    gifsicle: {
-                        colors: 256,
-                        interlaced: false,
-                        optimizationLevel: 3
-                    },
-                    optipng: {
-                        optimizationLevel: 7
-                    },
-                    svgo: svgo.config
-                }
             }
-            // {
-            //     test: /\.jpe?g$/,
-            //     loaders: [
-            //         {
-            //             loader: 'lqip-loader',
-            //             options: {
-            //                 path: '/media', // your image going to be in media folder in the output dir 
-            //                 name: '[name].[ext]' // you can use [hash].[ext] too if you wish 
-            //             }
-            //         },
-            //     ]
-            // }
         ],
         exclude: [
             resolve(define.rs_root, '/assets/fonts'),
             resolve(define.rs_root, '/assets/svgstore')
         ]
     },
-    // {
-    //     test: /\.svg$/i,
-    //     use: [
-    //         {
-    //             loader: 'babel-loader'
-    //         },
-    //         {
-    //             loader: 'react-svg-loader',
-    //             query: {
-    //                 svgo: svgo.config
-    //             }
-    //         },
-    //         {
-    //             loader: 'svg-sprite-loader',
-    //             options: {
-    //                 extract: true,
-    //                 // emitFile: false,
-    //                 spriteFilename: 'svgstore.svg'
-    //             }
-    //         },
-    //         // {
-    //         //     loader: 'svg-fill-loader',
-    //         //     options: {
-    //         //         raw: false,
-    //         //         selector: 'path,circle'
-    //         //     }
-    //         // },
-    //         {
-    //             loader: 'svgo-loader',
-    //             options: svgo.config
-    //         }
-    //     ],
-    //     // include: [ resolve(define.rs_root, '/assets/svgstore') ]
-    // }
+    {
+        enforce: 'pre',
+        test: /.*\.(jpe?g|png|gif|ico|webp|svg)$/i,
+        options: {
+            bypassOnDebug: define.rs_development,
+            mozjpeg: {
+                quality: 70,
+                progressive: true
+            },
+            pngquant:{
+                quality: '65-90',
+                speed: 4
+            },
+            gifsicle: {
+                colors: 256,
+                interlaced: false,
+                optimizationLevel: 3
+            },
+            optipng: {
+                optimizationLevel: 7
+            },
+            svgo: svgo.config
+        },
+        loader: 'image-webpack-loader',
+        exclude: [
+            resolve(define.rs_root, '/assets/fonts'),
+            resolve(define.rs_root, '/assets/svgstore')
+        ]
+    }
 ];
 
 module.exports.config = rules;
