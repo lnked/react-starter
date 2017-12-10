aec35eec6b691f82e30b3cb204ea7df1fdcb4b45
ed <ed.proff@gmail.com>
2017-12-11 01:17:09 +0300

    upgrade dependencies

--- webpack/rules/images.js
+++ webpack/rules/images.js
@@ -51,6 +51,45 @@ const rules = [
             resolve(define.rs_root, '/assets/svgstore')
         ]
     }
+    // {
+    //     /**
+    //      * OPTION A:
+    //      * default file-loader fallback
+    //      **/
+    //     test: /\.jpe?g$/,
+    //     loaders: [
+    //         {
+    //             loader: 'lqip-loader',
+    //             options: {
+    //                 path: '/media', // your image going to be in media folder in the output dir
+    //                 name: '[name].[ext]', // you can use [hash].[ext] too if you wish,
+    //                 base64: true, // default: true, gives the base64 encoded image
+    //                 palette: true, // default: false, gives the dominant colours palette
+    //             }
+    //         },
+    //     ]
+
+    //     /**
+    //      * OPTION B:
+    //      * Chained with your own url-loader or file-loader
+    //      **/
+    //     test: /\.(png|jpe?g)$/,
+    //     loaders: [
+    //       {
+    //         loader: 'lqip-loader',
+    //         options: {
+    //           base64: true,
+    //           palette: false
+    //         }
+    //       },
+    //       {
+    //         loader: "url-loader",
+    //         options: {
+    //           limit: 8000
+    //         }
+    //       }
+    //   ]
+    // }
 ];
 
 module.exports.config = rules;
