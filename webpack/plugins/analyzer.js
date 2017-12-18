3fa9491c38f55ac4010cf45b4023ef918b164d36
ed <ed.proff@gmail.com>
2017-12-18 21:19:54 +0300

    upgrade

--- webpack/plugins/analyzer.js
+++ webpack/plugins/analyzer.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const webpack = require('webpack');
+const define = require('./define');
+const { resolve } = require('path');
 
 const WebpackBundleSizeAnalyzerPlugin = require('webpack-bundle-size-analyzer').WebpackBundleSizeAnalyzerPlugin;
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
@@ -21,14 +23,14 @@ const plugins = [
         analyzerPort: 8888,
         // Path to bundle report file that will be generated in `static` mode.
         // Relative to bundles output directory.
-        reportFilename: 'report.html',
+        reportFilename: resolve(define.rs_dist, 'report.html'),
         // Automatically open report in default browser
         openAnalyzer: true,
         // If `true`, Webpack Stats JSON file will be generated in bundles output directory
-        generateStatsFile: false,
+        generateStatsFile: true,
         // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
         // Relative to bundles output directory.
-        statsFilename: 'stats.json',
+        statsFilename: resolve(define.rs_dist, 'stats.json'),
         // Options for `stats.toJson()` method.
         // For example you can exclude sources of your modules from stats file with `source: false` option.
         // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
