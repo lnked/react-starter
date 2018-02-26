2c55ec4f64199289626f804a6c3afebde5f3a91c
Celebro dev <ed.proff@gmail.com>
2018-02-26 14:57:25 +0300

    update dependencies

--- app/containers/updates/index.tsx
+++ app/containers/updates/index.tsx
@@ -26,36 +26,40 @@ export default class Updates extends React.Component<{}, {}> {
     }
 
     _createReleaseItem (release: any) {
-        console.log(release)
-        return (<div />)
-        // return (
-        //     <li className={css.post__item} key={post.id}>
-        //         {['self', 'default'].indexOf(post.thumbnail) < 0 && (
-        //             <div>
-        //                 <img src={post.thumbnail} alt="" />
-        //             </div>
-        //         )}
-        //         <Link to={post.url} target="_blank" rel="noopener">{post.title}</Link>
-        //         <p>{post.author_flair_text}</p>
-        //     </li>
-        // )
+        const notes = []
+
+        Object.keys(release.notes).map(key => {
+            const items = release.notes[key]
+            console.log(key, items)
+
+            items.map((note: any, index: number) => {
+                notes.push(<li className={css[key]} key={`${key}.${index}`}>{note}</li>)
+            })
+        })
+
+        return (
+            <div className={css.release} key={release.version}>
+                <div className={css.version}>{release.version}</div>
+                <div className={css.date}>{release.date}</div>
+                <ul className={css.notes}>{notes}</ul>
+            </div>
+        )
     }
 
     render () {
+        const list = []
         const { releases } = this.state
 
-        console.log('releases: ', releases)
-        console.log('releases list: ', releases.list)
+        if (releases.hasOwnProperty('list')) {
+            releases.list.forEach(release => {
+                list.push(this._createReleaseItem(release))
+            })
+        }
 
-        // (this._createReleaseItem) }
-        // {
-        //     releases.list.forEach(release => {
-        //         console.log(release)
-        //     })
-        // }
         return (
             <div className={css.list}>
-                <h1>{releases.count} update available</h1>
+                <h1 className={css.count}>{releases.count} update available</h1>
+                { list }
             </div>
         )
     }
