07badeb2b700ddb4bb16212207d80726659e3e39
Celebro dev <ed.proff@gmail.com>
2018-03-09 23:26:32 +0300

    fix

--- app/containers/dashboard/index.tsx
+++ app/containers/dashboard/index.tsx
@@ -141,7 +141,7 @@ export default class Dashboard extends React.Component<{}, {}> {
                         {
                             label: 'Третий',
                             value: 3
-    s                    },
+                        },
                         {
                             label: 'Четвертый',
                             value: 4
