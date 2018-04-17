02b2e558ec9dfb3ccfc7b05e6adca76abd56e985
Celebro dev <ed.proff@gmail.com>
2018-04-18 02:10:59 +0300

    update dependencies

--- app/svgstore.jsx
+++ app/svgstore.jsx
@@ -1,6 +1,6 @@
-const __svg__ = {
-    path: 'assets/svgstore/**/*.svg',
-    name: '../.cache/svgstore/svgstore.svg'
-}
+// const __svg__ = {
+//     path: 'assets/svgstore/**/*.svg',
+//     name: '../.cache/svgstore/svgstore.svg'
+// }
 
-require('webpack-svgstore-plugin/src/helpers/svgxhr')(__svg__)
+// require('webpack-svgstore-plugin/src/helpers/svgxhr')(__svg__)
