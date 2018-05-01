d11eddae22bdb9111b4d8d12d214cd5430922085
Celebro dev <ed.proff@gmail.com>
2018-05-01 19:57:50 +0300

    update

--- src/preload.tsx
+++ src/preload.tsx
@@ -1,5 +1,49 @@
 // import * as React from 'react'
 
+// enum Transport {
+//     train,
+//     bus,
+//     bike,
+//     car
+// }
+
+// let myTransport = Transport.bus
+// console.log(myTransport)
+// enum Students {
+// Ann,
+// Bob,
+// John,
+// Zed
+// }
+
+// console.log(Students['John']); // 2
+
+// console.log(Students[3]); // Zed
+
+// enum Tools {
+//     hammer,
+//     screwdriver,
+//     saw = 100,
+//     wrench
+// }
+
+//   console.log(Tools.wrench); // 101
+
+// enum Sports {
+// Soccer,
+// Football,
+// Basketball,
+// }
+
+// // ...
+
+// // Later or in another file:
+// enum Sports {
+// Swimming = 3,
+// Running,
+// Hockey
+// }
+
 // export default class FeedbackButton extends React.Component {
 //     handleButtonClick () {
 //         // ↓ Here, import() will make webpack split FeedbackModal
