b8f284da465c4289f90f4ab40befba1be703db77
Celebro dev <ed.proff@gmail.com>
2018-05-12 15:38:37 +0300

    upgrade dependencies

--- webpack/plugins/production.js
+++ webpack/plugins/production.js
@@ -5,13 +5,13 @@ const glob = require('glob');
 const webpack = require('webpack');
 const define  = require('../define');
 
-const { GuessPlugin } = require('guess-webpack');
-const environment = require('../environment').config;
+// const { GuessPlugin } = require('guess-webpack');
+// const environment = require('../environment').config;
 const WebpackChunkHash = require('webpack-chunk-hash');
 const WebpackManifestPlugin = require('webpack-manifest-plugin');
 
 const plugins = [
-    GuessPlugin({ GA: environment.GOOGLE_ANALYTICS_ID }),
+    // GuessPlugin({ GA: environment.GOOGLE_ANALYTICS_ID }),
 
     new WebpackManifestPlugin({
         basePath: define.rs_output_path,
