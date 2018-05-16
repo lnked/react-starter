cdecb67823b8f586d911886500e69cc43e6abef2
Celebro dev <ed.proff@gmail.com>
2018-05-16 05:35:27 +0300

    fix html minify

---
+++ src/helpers/get.js
@@ -0,0 +1,13 @@
+// static get(action, params = {}) {
+//     return new Promise((resolve, reject) => {
+//         fetch(UrlHelper.apiUrl(action, params), {
+//             credentials: 'same-origin'
+//         })
+//         .then(response => {
+//             return Api.checkStatus(resolve, reject, response)
+//         })
+//         .then(([ ok, response ]) => {
+//             return ok ? resolve(response) : reject(response);
+//         });
+//     });
+// }
