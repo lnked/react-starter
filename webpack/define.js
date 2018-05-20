0ae22f0d1885c949100dd6c40a59d795fe41571d
Celebro dev <ed.proff@gmail.com>
2018-05-20 21:47:40 +0300

    fix html minify

--- webpack/define.js
+++ webpack/define.js
@@ -20,6 +20,25 @@ const isDevelopment = process.argv.includes('development') || !isProduction;
 const _host_ = process.env.HOST || '0.0.0.0';
 const _port_ = process.env.PORT || 3000;
 
+// const signale = require('signale');
+
+// signale.success('Operation successful');
+// signale.debug('Hello', 'from', 'L59');
+// signale.pending('Write release notes for 1.2.0');
+// signale.fatal(new Error('Unable to acquire lock'));
+// signale.watch('Recursively watching build directory...');
+// signale.complete({prefix: '[task]', message: 'Fix issue #59', suffix: '(@klauscfhq)'});
+
+// signale.time('test');
+// signale.time();
+// signale.time();
+
+// setTimeout(() => {
+//     signale.timeEnd();
+//     signale.timeEnd();
+//     signale.timeEnd('test');
+// }, 500);
+
 module.exports = {
     rs_host: _host_,
     rs_port: _port_,
