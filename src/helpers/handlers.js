97b02d1df485b67b4d149b2b810915dc9fffb996
Celebro dev <ed.proff@gmail.com>
2018-06-01 01:42:40 +0300

    update

---
+++ src/helpers/handlers.js
@@ -0,0 +1,28 @@
+export const createMarkup = (data) => {
+    return {__html: data}
+}
+
+export const ucfirst = (str) => {
+    const f = str.charAt(0).toUpperCase()
+    return f + str.substr(1, str.length - 1)
+}
+
+export const formatMoney = (num, penny) => {
+    if (typeof penny === 'undefined') {
+        penny = false
+    }
+
+    const formatter = new Intl.NumberFormat('ru')
+
+    return formatter.format(num)
+}
+
+export const formatDeclOfNum = (n, titles) => {
+    const cases = [2, 0, 1, 1, 1, 2]
+    n = Math.abs(n)
+    return titles[(n % 100 > 4 && n % 100 < 20) ? 2 : cases[(n % 10 < 5) ? n % 10 : 5]]
+}
+
+export const declOfNum = (n, titles) => {
+    return [n, formatDeclOfNum(n, titles)].join(' ')
+}
---
+++ src/helpers/predicts.js
@@ -0,0 +1,6 @@
+export const isActive = (match, location) => {
+    if (!match) {
+        return false
+    }
+    return location.pathname === match.path
+}
