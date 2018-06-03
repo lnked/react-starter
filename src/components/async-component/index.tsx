de51e5b6fd5b921a08ec25ed02cc5e3248594372
Celebro dev <ed.proff@gmail.com>
2018-06-04 01:18:07 +0300

    update

---
+++ src/components/async-component/index.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+
+interface S {
+    Component: any;
+}
+
+export default (loader, collection) => (class AsyncComponent extends React.Component<{}, S> {
+    state = {
+        Component: null
+    }
+
+    componentDidMount () {
+        if (!this.state.Component) {
+            loader().then((Component) => {
+                this.setState({ Component })
+            })
+        }
+    }
+
+    render () {
+        const { Component } = this.state
+
+        if (Component) {
+            return (
+                // @ts-ignore
+                <Component { ...this.props } { ...collection } />
+            )
+        }
+
+        return null
+    }
+})
