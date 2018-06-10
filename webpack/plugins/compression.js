c0a76596ab0b92cf3ee56bfa477e2702d5981af2
Celebro dev <ed.proff@gmail.com>
2018-06-10 23:19:45 +0300

    update

--- webpack/plugins/compression.js
+++ webpack/plugins/compression.js
@@ -1,30 +1,31 @@
 'use strict';
 
-// const BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
+const BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 const plugins = [
-    // new BrotliGzipPlugin({
-    //     asset: '[path].br[query]',
-    //     algorithm: 'brotli',
-    //     test: /\.(js|css|html|svg)$/,
-    //     threshold: 10240,
-    //     minRatio: 0.8
-    // }),
-    // new BrotliGzipPlugin({
+    new BrotliGzipPlugin({
+        asset: '[path].br[query]',
+        algorithm: 'brotli',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8,
+        quality: 11
+    }),
+    new BrotliGzipPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8
+    }),
+    // new CompressionPlugin({
     //     asset: '[path].gz[query]',
     //     algorithm: 'gzip',
     //     test: /\.(js|css|html|svg)$/,
     //     threshold: 10240,
     //     minRatio: 0.8
     // })
-    new CompressionPlugin({
-        asset: '[path].gz[query]',
-        algorithm: 'gzip',
-        test: /\.(js|css|html|svg)$/,
-        threshold: 10240,
-        minRatio: 0.8
-    })
 ];
 
 module.exports.config = plugins;
