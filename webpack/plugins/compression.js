8fa6358fb400548b60de99322a0ece6a7d9d365e
Celebro dev <ed.proff@gmail.com>
2018-06-10 23:30:46 +0300

    update

--- webpack/plugins/compression.js
+++ webpack/plugins/compression.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
+// const CompressionPlugin = require('compression-webpack-plugin');
 
 const plugins = [
     new BrotliGzipPlugin({
@@ -18,7 +18,7 @@ const plugins = [
         test: /\.(js|css|html|svg)$/,
         threshold: 10240,
         minRatio: 0.8
-    }),
+    })
     // new CompressionPlugin({
     //     asset: '[path].gz[query]',
     //     algorithm: 'gzip',
