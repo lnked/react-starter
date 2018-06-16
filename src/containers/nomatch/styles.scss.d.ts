adc9df7e26b4d8d0423a80115e9946c814c93f34
Celebro dev <ed.proff@gmail.com>
2018-06-16 04:05:14 +0300

    update

--- src/containers/nomatch/styles.scss.d.ts
+++ src/containers/nomatch/styles.scss.d.ts
@@ -1,6 +1,5 @@
 export const error: string;
 export const figure: string;
-export const figure__image: string;
 export const figureImage: string;
 export const content: string;
 export const title: string;
