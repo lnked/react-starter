2332d42c3d39424fa5a86e7b667bba84fb23d9fd
Celebro dev <ed.proff@gmail.com>
2018-06-28 21:36:20 +0300

    update

--- src/containers/nomatch/index.tsx
+++ src/containers/nomatch/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import * as css from './styles.scss'
+import classNames from 'classnames/bind'
 
 export interface Location {
     pathname: string;
@@ -9,22 +10,20 @@ export interface P {
     location: Location;
 }
 
-export function NoMatch ({ location }: P) {
+const cx = classNames.bind(css)
+
+export const NoMatch = ({ location }: P) => {
     document.title = '404 Not Found'
 
     return (
-        <div className={css.error}>
-            <div className={css.figure}>
-                <img
-                    src={require('images/logo.svg')}
-                    className={css.figureImage}
-                    alt="Error: 4xx"
-                />
+        <div className={cx({ error: true })}>
+            <div className={cx({ figure: true })}>
+                <img src={require('images/logo.svg')} className={cx({ figureImage: true })} alt="Error: 4xx" />
             </div>
 
-            <div className={css.content}>
-                <h1 className={css.title}>Ошибка (4xx)</h1>
-                <p className={css.description}>
+            <div className={cx({ content: true })}>
+                <h1 className={cx({ title: true })}>Ошибка (4xx)</h1>
+                <p className={cx({ description: true })}>
                     Не удалось найти страницу <code>{location.pathname}</code>.
                 </p>
             </div>
