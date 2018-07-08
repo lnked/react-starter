1dc2bf6cebbea639d8cb078c657de893f94b8073
Celebro dev <ed.proff@gmail.com>
2018-07-08 11:56:28 +0300

    remove image

--- src/typings/global.ts
+++ src/typings/global.ts
@@ -1,9 +1,11 @@
 declare var __VERSION__: string
 declare var __IN_DEBUG__: boolean
 
+declare module 'hocs'
 declare module 'store'
 declare module 'utils'
 declare module 'theme'
+declare module 'helpers'
 declare module 'segments'
 declare module 'settings'
 declare module 'components'
