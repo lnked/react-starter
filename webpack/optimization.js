3d4612c4c4a5e5a628e5993a89ad34f91c01a422
Celebro dev <ed.proff@gmail.com>
2018-07-09 00:22:37 +0300

    fix clearable

--- webpack/optimization.js
+++ webpack/optimization.js
@@ -57,4 +57,32 @@ module.exports = {
             }
         }
     }
+
+    // splitChunks: {
+    //   chunks: 'all',
+    //   cacheGroups: {
+    //     default: false,
+    //     // Custom common chunk
+    //     bundle: {
+    //       name: 'commons',
+    //       chunks: 'all',
+    //       minChunks: 3,
+    //       reuseExistingChunk: false,
+    //     },
+    //     // Customer vendor
+    //     vendor: {
+    //       filename: 'vendorbundle.js',
+    //       test: m => /node_modules/.test(m.context),
+    //     },
+    //     // Merge all the CSS into one file
+    //     styles: {
+    //       name: 'styles',
+    //       test: /\.s?css$/,
+    //       chunks: 'all',
+    //       minChunks: 1,
+    //       reuseExistingChunk: true,
+    //       enforce: true,
+    //     },
+    //   },
+    // },
 };
