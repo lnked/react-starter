4d8ed086eea263beec65c7038f64cc0e7495ff44
Celebro dev <ed.proff@gmail.com>
2018-07-17 01:19:55 +0300

    upgrade autoprefixer

--- src/__tests__/predicts.test.js
+++ src/__tests__/predicts.test.js
@@ -1,11 +1,11 @@
 import {
-    isInt
+    isInt,
     // isNull,
     // isPhone,
     // isFloat,
     // compare,
     // isActive,
-    // isUndefined
+    isUndefined
 } from 'helpers/predicts'
 
 test('predicts', () => {})
@@ -14,15 +14,15 @@ test('isInt(1) to equal true', () => {
     expect(isInt(1)).toBeTruthy()
 })
 
-// test('isInt("1") to equal false', () => {
-//     expect(isInt('1')).toBeFalsy()
-// })
+test('isInt("1") to equal false', () => {
+    expect(isInt('1')).toBeFalsy()
+})
 
-// test('isUndefined(param) to equal true', () => {
-//     expect(isUndefined(param)).toBeTruthy()
-// })
+test('isUndefined(param) to equal true', () => {
+    expect(isUndefined(undefined)).toBeTruthy()
+})
 
-// test('isUndefined(param) to equal true', () => {
-//     const param = ''
-//     expect(isUndefined(param)).toBeFalsy()
-// })
+test('isUndefined(param) to equal true', () => {
+    const param = ''
+    expect(isUndefined(param)).toBeFalsy()
+})
