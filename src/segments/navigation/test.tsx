861f2084769d23565acc91ec13485b65e5e34f28
Celebro dev <ed.proff@gmail.com>
2018-07-22 01:37:50 +0300

    add .babelrc.js

---
+++ src/segments/navigation/test.tsx
@@ -0,0 +1,12 @@
+// import React from "react";
+// import { withRouter } from "react-router-dom";
+
+// const Nav = ({ history }) => (
+//  <nav>
+//   <div onClick={() => history.push('/')}>Истории</div>
+//   <div onClick={() => history.push('/bilboard')}>Билборд</div>
+//   <div onClick={() => history.push('/loved')}>Избранное</div>
+//  </nav>
+// )
+
+// export default withRouter(Nav);
