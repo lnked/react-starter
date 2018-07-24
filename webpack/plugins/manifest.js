abcde68c668ba443002090894fe5406773b2219c
Celebro dev <ed.proff@gmail.com>
2018-07-24 13:35:04 +0300

    update

--- webpack/plugins/manifest.js
+++ webpack/plugins/manifest.js
@@ -12,7 +12,7 @@ const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const plugins = [
     new FaviconsWebpackPlugin({
         logo: resolve(define.rs_root, 'assets/favicon/favicon.svg'),
-        prefix: '_',
+        prefix: '/',
         inject: true,
         emitStats: false,
         statsFilename: '[hash:4].json',
@@ -34,24 +34,32 @@ const plugins = [
     }),
     new WebpackPwaManifest({
         filename: "../manifest.webmanifest",
-        // includeDirectory: true,
-        name: JSON.parse(environment.APP_NAME),
-        short_name: JSON.parse(environment.APP_NAME_SHORT),
-        description: JSON.parse(environment.APP_NAME_DESC),
-        background_color: JSON.parse(environment.APP_BG_COLOR),
-        dir: 'auto',
-        lang: 'ru-RU',
-        display: 'standalone',
-        orientation: 'any',
-        start_url: '/index.html',
-        theme_color: JSON.parse(environment.APP_THEME_COLOR),
+        includeDirectory: true,
         inject: false,
         fingerprints: false,
         ios: false,
         publicPath: null,
-        theme_color: JSON.parse(environment.APP_BG_COLOR),
+        name: JSON.parse(environment.APP_NAME),
+        dir: 'auto',
+        lang: 'ru-RU',
+        display: 'standalone',
+        orientation: 'any',
+        description: JSON.parse(environment.APP_NAME_DESC),
+        start_url: '/index.html',
+        short_name: JSON.parse(environment.APP_NAME_SHORT),
+        theme_color: JSON.parse(environment.APP_THEME_COLOR),
+        background_color: JSON.parse(environment.APP_BG_COLOR),
         prefer_related_applications: false,
-        related_applications: [],
+        related_applications: [
+            // {
+            //     "platform": "play",
+            //     "url": "https://play.google.com/store/apps/details?id=ru.agroclub"
+            // },
+            // {
+            //     "platform": "itunes",
+            //     "url": "https://itunes.apple.com/us/app/агро-клуб/id1401935493?ls=1&mt=8"
+            // }
+        ],
         icons: [
             {
                 src: resolve(define.rs_root, 'assets/favicon/favicon.svg'),
