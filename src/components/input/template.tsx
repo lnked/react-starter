56c052faf49cceda1dd889c9b5664eafd3ecf60c
Celebro dev <ed.proff@gmail.com>
2018-07-28 02:28:23 +0300

    upgrade dependencies

--- src/components/input/template.tsx
+++ src/components/input/template.tsx
@@ -5,19 +5,19 @@ import classNames from 'classnames/bind'
 
 export interface P {
     value: string;
+    label: string;
     handleChange: (e: any) => void;
 }
 
 const cx = classNames.bind(css)
 
-export const Template = ({ value, handleChange }) => {
-    return (
-        <div className={cx({ wrapper: true })}>
-            <p>value: {value}</p>
-            <input
-                value={value}
-                onChange={handleChange}
-            />
-        </div>
-    )
-}
+export const Template = ({ value, label, handleChange }: P) => (
+    <div className={cx({ wrapper: true })}>
+        <p>{label}</p>
+        <p>value: {value}</p>
+        <input
+            value={value}
+            onChange={handleChange}
+        />
+    </div>
+)
