792b55f7c6dd8961c7cbfb2e016db69cefe06067
Celebro dev <ed.proff@gmail.com>
2018-07-30 23:20:44 +0300

    update

--- src/app.tsx
+++ src/app.tsx
@@ -27,7 +27,7 @@ const history = createBrowserHistory()
 const rootStore = createStore(history, initialState)
 
 export class App extends React.Component<{}, {}> {
-    renderDevTool () {
+    renderDevTool = () => {
         if (process.env.NODE_ENV !== 'production') {
             const DevTools = require('mobx-react-devtools').default
             return <DevTools />
