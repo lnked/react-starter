e20934a79a3ac6df12ee3c9ebc28ab309d3d58e5
Celebro dev <ed.proff@gmail.com>
2018-08-06 00:33:09 +0300

    updatE

---
+++ src/compound/list/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+export class List extends React.Component<{}, {}> {
+    // deleteItem = indexToDelete => {
+    //     this.setState(({ list }) => ({
+    //       list: list.filter((toDo, index) => index !== indexToDelete)
+    //     }))
+    // }
+
+    render () {
+        return null
+        // return <ToDoItem deleteItem={this.deleteItem.bind(this, key)}/>
+    }
+}
