5d56bc4aefbe5c2f8ac44a4ea0c39e09af2179c1
Celebro dev <ed.proff@gmail.com>
2018-08-07 11:37:36 +0300

    upgrade dependencies / light

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-
 import { TOKEN } from './token'
 
 export const API_URL = process.env.REACT_APP_API_URL
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,1 +1,4 @@
+export * from './api'
 export * from './сlasses'
+export * from './predicts'
+export * from './handlers'
