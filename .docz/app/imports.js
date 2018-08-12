e5db29957410faa2224bda8630844469867be067
Celebro dev <ed.proff@gmail.com>
2018-08-13 00:54:52 +0300

    pwa test

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'src/docz/hello.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-docz-hello" */ 'src/docz/hello.mdx'),
-  'src/components/button/button.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-button-button" */ 'src/components/button/button.mdx'),
-}
