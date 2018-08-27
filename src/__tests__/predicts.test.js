45282d2dc5968a588d4e0988a61370400ae35951
Celebro dev <ed.proff@gmail.com>
2018-08-27 12:11:30 +0300

    update

--- src/__tests__/predicts.test.js
+++ src/__tests__/predicts.test.js
@@ -1,22 +1,22 @@
-// import {
-//     isInt
-//     // isNull,
-//     // isPhone,
-//     // isFloat,
-//     // compare,
-//     // isActive,
-//     // isUndefined
-// } from 'helpers/predicts'
+import {
+    isInt,
+    // isNull,
+    // isPhone,
+    // isFloat,
+    // compare,
+    // isActive,
+    // isUndefined,
+} from 'helpers/predicts'
 
 test('test', () => {})
 
-// test('isInt(1) to equal true', () => {
-//     expect(isInt(1)).toBeTruthy()
-// })
+test('isInt(1) to equal true', () => {
+    expect(isInt(1)).toBeTruthy()
+})
 
-// test('isInt("1") to equal false', () => {
-//     expect(isInt('1')).toBeFalsy()
-// })
+test('isInt("1") to equal false', () => {
+    expect(isInt('1')).toBeFalsy()
+})
 
 // test('isUndefined(param) to equal true', () => {
 //     expect(isUndefined('undefined')).toBeTruthy()
