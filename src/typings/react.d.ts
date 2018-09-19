9415bf75d123fb8d3b2ff8835f3ed5fe317627c9
Edik Bulikyan <ed.proff@gmail.com>
2018-09-19 05:01:24 +0300

    update

--- src/typings/react.d.ts
+++ src/typings/react.d.ts
@@ -45,10 +45,9 @@ type NodeModule = {
         accept(dependencies: string[], callback: (updatedDependencies: string[]) => void): void;
         accept(dependency: string, callback: () => void): void;
         accept(errHandler?: (err: any) => void): void;
-        decline(dependencies: string[]): void;
-        decline(dependency: string): void;
-        decline(): void;
-
+        decline(dependencies?: string[]): void;
+        // decline(dependency: string): void;
+        // decline(): void;
         dispose(callback: (data: any) => void): void;
         addDisposeHandler(callback: (data: any) => void): void;
 
