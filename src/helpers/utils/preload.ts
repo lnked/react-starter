14414d58aa9758b4f20bfd0dd6058d93d58c9dbb
Edik Bulikyan <ed.proff@gmail.com>
2018-09-24 02:32:06 +0300

    update

--- src/helpers/utils/preload.ts
+++ src/helpers/utils/preload.ts
@@ -1,19 +1,13 @@
-export const preload = (stack: any[], pending = false): void => {
-    const cb = (data: any, index: number = 0) => {
-        const fn = stack.shift()
-
-        if (typeof fn === 'function') {
-            fn(cb, data)
-        }
-
-        if (index === 0 && pending) {
-            console.log('with pending')
-        }
-
-        index--
-    }
-
+export const preload = (stack: any[]): void => {
     if (stack && stack.length) {
-        cb(null, stack.length)
+        const cb = (data: any) => {
+            const fn = stack.shift()
+
+            if (typeof fn === 'function') {
+                fn(cb, data)
+            }
+        }
+
+        cb(null)
     }
 }
