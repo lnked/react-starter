35bd5d9505782bdf2794678e411c636f570a34f6
Edik Bulikyan <ed.proff@gmail.com>
2018-09-26 03:44:03 +0300

    deals & meetings

--- src/fragments/locale/index.tsx
+++ src/fragments/locale/index.tsx
@@ -8,12 +8,12 @@ import { Link } from 'react-router-dom'
 export interface P {
     checked?: string;
     className?: string;
-    handleChange?: () => void | boolean;
+    // handleChange?: () => void | boolean;
 }
 
 const cx = classes.bind(css)
 
-export const Locale = ({ checked, className = '', handleChange = false }: P) => {
+export const Locale = ({ checked, className = '' }: P) => {
     const list: string[] = ['ru', 'en', 'de']
 
     return (
@@ -23,7 +23,6 @@ export const Locale = ({ checked, className = '', handleChange = false }: P) => 
                     <Link
                         key={lang}
                         to={`/${lang}`}
-                        onClick={handleChange}
                         className={cx(css.link, { active: checked === lang })}>
                         {lang}
                     </Link>
