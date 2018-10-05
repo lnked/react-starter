c1be14bb04697da94dcade98133860147b7d6a9a
Edik Bulikyan <ed.proff@gmail.com>
2018-10-06 00:20:55 +0300

    fix trim

--- src/helpers/utils/trim.ts
+++ src/helpers/utils/trim.ts
@@ -1,10 +1,18 @@
 import { isUndefined } from 'helpers'
 
 export const trim = (str: string): string =>
-    str && str.replace(/^\s+|\s+$/g, '')
+    str && str.toString().replace(/^\s+|\s+$/g, '')
 
 export const clean = (str: string): string =>
-    (str && str.replace(/\s+/gm, '').replace(/_/gm, '').replace(/-/gm, '').replace(/[()]+/gm, '')) || ''
+    (
+        str &&
+            str
+                .toString()
+                .replace(/\s+/gm, '')
+                .replace(/_/gm, '')
+                .replace(/-/gm, '')
+                .replace(/[()]+/gm, '')
+    ) || ''
 
 export const trimmed = (str: string): string =>
-    str && (!isUndefined(str) ? clean(str).toString() : '')
+    str && (!isUndefined(str) ? clean(str || '').toString() : '')
