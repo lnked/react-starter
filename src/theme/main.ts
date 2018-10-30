d0040a36ad9b61ddb2238ceddd158618987652d2
Edik Bulikyan <ed.proff@gmail.com>
2018-10-30 10:11:34 +0300

    upgrade dependencies

---
+++ src/theme/main.ts
@@ -0,0 +1,15 @@
+export const type = {
+    base: 'IBMPlexSans',
+    medium: 'IBMPlexSans-Medium',
+    semiBold: 'IBMPlexSans-SemiBold',
+    italic: 'IBMPlexSans-Italic'
+}
+
+export const size = {
+    tiny: 10,
+    small: 12,
+    normal: 14,
+    big: 16,
+    large: 24,
+    extraLarge: 32
+}
