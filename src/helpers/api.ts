4cf848312235f9dfc15a604efe63d4557859932c
Edik Bulikyan <ed.proff@gmail.com>
2018-11-19 13:17:50 +0300

    update

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,4 +1,6 @@
-export const API_URL = process.env.REACT_APP_API_URL
-export const DADATA_API_KEY = process.env.DADATA_API_KEY
-export const DADATA_COMPANY_API_URL = process.env.DADATA_COMPANY_API_URL
-export const DADATA_ADDRESS_API_URL = process.env.DADATA_ADDRESS_API_URL
+export const {
+    DADATA_API_KEY,
+    DADATA_COMPANY_API_URL,
+    DADATA_ADDRESS_API_URL,
+    REACT_APP_API_URL: API_URL,
+} = process.env
