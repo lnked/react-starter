52b9a96bf547b07015ed17a56be5730d72a4b71d
Edik Bulikyan <ed.proff@gmail.com>
2018-11-21 18:10:56 +0300

    update routes

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
-export { routes as MainPage } from './main-page/route'
-export { routes as PanelsPage } from './panels-page/route'
-export { routes as ExamplePage } from './example-page/route'
+export { routes as MainRoute } from './main-page/route'
+export { routes as PanelsRoute } from './panels-page/route'
+export { routes as ExampleRoute } from './example-page/route'
 
 // Should always be the last
-export { routes as NoMatchPage } from './nomatch-page/route'
+export { routes as NoMatchRoute } from './nomatch-page/route'
