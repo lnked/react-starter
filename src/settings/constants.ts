c75221e061206d34fadaa3b62f966a1ae80f6a06
Edik Bulikyan <ed.proff@gmail.com>
2018-11-26 09:51:32 +0300

    update

--- src/settings/constants.ts
+++ src/settings/constants.ts
@@ -3,30 +3,30 @@ export const STORE_APP = 'app'
 export const STORE_ROUTER = 'router'
 
 // export enum ActionType {
-//     init = 'init',
-//     buyGood = 'buyGood',
-//     removeGood = 'removeGood',
-//     deleteGood = 'deleteGood',
-//     fillCat = 'fillCat',
-//     fillGoods = 'fillGoods',
-//     order = 'order',
-//     emptyCart = 'emptyCart',
+//   init = 'init',
+//   buyGood = 'buyGood',
+//   removeGood = 'removeGood',
+//   deleteGood = 'deleteGood',
+//   fillCat = 'fillCat',
+//   fillGoods = 'fillGoods',
+//   order = 'order',
+//   emptyCart = 'emptyCart',
 // };
 
 // enum A {
-//     a = 'a',
-//     b = 'b',
-//     c = 'c'
+//   a = 'a',
+//   b = 'b',
+//   c = 'c'
 // }
 
 // trying to type generic vars
 // interface Entity {
-//     id: number;
-//     parentId: number;
-//     state: string[]
-//     path: string;
-//     route: string;
-//     lazyPath?: string;
+//   id: number;
+//   parentId: number;
+//   state: string[]
+//   path: string;
+//   route: string;
+//   lazyPath?: string;
 // }
 
 // type Entities<T> = { [key in keyof T]: Entity }
@@ -34,18 +34,18 @@ export const STORE_ROUTER = 'router'
 // type RoutesEntity<T> = { [key in keyof T]: Entities<any> }
 
 // interface Nums {
-//     one;
-//     two;
-//     three;
+//   one;
+//   two;
+//   three;
 // }
 
 // interface Strs {
-//     a;
-//     b;
-//     c;
+//   a;
+//   b;
+//   c;
 // }
 
 // interface MyEntity {
-//     nums: Entities<Nums>;
-//     strs: Entities<Strs>;
+//   nums: Entities<Nums>;
+//   strs: Entities<Strs>;
 // }
