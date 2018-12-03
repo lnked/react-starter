c80254360e852f55ac37b654fdc55a5570274593
Edik Bulikyan <ed.proff@gmail.com>
2018-12-03 12:34:10 +0300

    update

--- src/helpers/utils/request.ts
+++ src/helpers/utils/request.ts
@@ -4,35 +4,31 @@ import { TOKEN } from '../token'
 import { API_URL } from '../api'
 
 const headers: any = {
-  'Accept': 'application/json',
-  'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
 }
 
-export const request: any = (options?: any): any => {
+export const request: any = (options?: any = {}): any => {
 
-  const authToken = TOKEN.get()
+    const noToken = Object.keys(options).length && options.hasOwnProperty('noToken') && options.noToken
 
-  if (authToken) {
+    if (!noToken) {
+        const authToken = TOKEN.get()
 
-    headers.Authorization = `Token ${authToken}`
+        if (authToken) {
+            headers.Authorization = `Token ${authToken}`
+        }
+    }
 
-  }
+    const config: any = {
+        baseURL: API_URL,
+        headers,
+    }
 
-  if (options && Object.keys(options).length && options.hasOwnProperty('noToken') && options.noToken) {
+    if (options) {}
 
-    Reflect.deleteProperty(headers, 'Authorization')
+    const instance = axios.create(config)
 
-  }
-
-  const config: any = {
-    baseURL: API_URL,
-    headers,
-  }
-
-  if (options) { }
-
-  const instance = axios.create(config)
-
-  return instance
+    return instance
 
 }
