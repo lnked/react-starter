5d3139219ff575047f7213c457f6a01f8bcaa832
Edik Bulikyan <ed.proff@gmail.com>
2018-12-03 12:59:43 +0300

    update

--- src/helpers/utils/request.ts
+++ src/helpers/utils/request.ts
@@ -4,31 +4,35 @@ import { TOKEN } from '../token'
 import { API_URL } from '../api'
 
 const headers: any = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
 }
 
 export const request: any = (options?: any = {}): any => {
 
-    const noToken = Object.keys(options).length && options.hasOwnProperty('noToken') && options.noToken
+  const noToken = Object.keys(options).length && options.hasOwnProperty('noToken') && options.noToken
 
-    if (!noToken) {
-        const authToken = TOKEN.get()
+  if (!noToken) {
 
-        if (authToken) {
-            headers.Authorization = `Token ${authToken}`
-        }
+    const authToken = TOKEN.get()
+
+    if (authToken) {
+
+      headers.Authorization = `Token ${authToken}`
+
     }
 
-    const config: any = {
-        baseURL: API_URL,
-        headers,
-    }
+  }
 
-    if (options) {}
+  const config: any = {
+    baseURL: API_URL,
+    headers,
+  }
 
-    const instance = axios.create(config)
+  if (options) {}
 
-    return instance
+  const instance = axios.create(config)
+
+  return instance
 
 }
