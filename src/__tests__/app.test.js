627f25fc21dea8312426c4fb0eb840ca64ea34a7
Edik Bulikyan <ed.proff@gmail.com>
2019-01-20 02:48:29 +0300

    upgrade dependencies

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,5 +1,15 @@
 test('App works', () => {})
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
 // import React from 'react'
 // import { render, fireEvent } from 'react-testing-library'
 // import App from '../App'
